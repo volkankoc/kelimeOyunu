327e4b5f83c94e733ac13151520aa1dbc000196a
volkankoc <volkankoc53@gmail.com>
2022-01-18 15:30:34 +0300

    kelime oyunu

---
+++ kelimeOyunu/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace kelimeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(397, 162);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 40);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(53, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 66);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(397, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(53, 93);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 66);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Enabled = false;
+            this.groupBox3.Location = new System.Drawing.Point(53, 165);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(267, 66);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Enabled = false;
+            this.groupBox4.Location = new System.Drawing.Point(53, 237);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(267, 66);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Enabled = false;
+            this.groupBox5.Location = new System.Drawing.Point(53, 309);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(267, 66);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Enabled = false;
+            this.groupBox6.Location = new System.Drawing.Point(53, 381);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(267, 66);
+            this.groupBox6.TabIndex = 6;
+            this.groupBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(479, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "KİTAP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 546);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kelimeOyunu/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kelimeOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TextBox kutu;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random a = new Random();
+            String buton = "b";
+          
+
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                   kutu = new TextBox();
+                    kutu.MaxLength = 1;
+                    kutu.Left = i * 50 + 15;
+                    kutu.Top = 20;
+                    kutu.Width = 36;
+                    kutu.Height = 40;
+                    kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                    kutu.Multiline = true;
+                    kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                    groupBox1.Controls.Add(kutu);
+
+                }
+            for (int i = 0; i < 5; i++)
+            {
+
+                kutu = new TextBox();
+                kutu.MaxLength = 1;
+                kutu.Left = i * 50 + 15;
+                kutu.Top = 20;
+                kutu.Width = 36;
+                kutu.Height = 40;
+                kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                kutu.Multiline = true;
+                kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                groupBox2.Controls.Add(kutu);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                kutu = new TextBox();
+                kutu.MaxLength = 1;
+                kutu.Left = i * 50 + 15;
+                kutu.Top = 20;
+                kutu.Width = 36;
+                kutu.Height = 40;
+                kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                kutu.Multiline = true;
+                kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                groupBox3.Controls.Add(kutu);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                kutu = new TextBox();
+                kutu.MaxLength = 1;
+                kutu.Left = i * 50 + 15;
+                kutu.Top = 20;
+                kutu.Width = 36;
+                kutu.Height = 40;
+                kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                kutu.Multiline = true;
+                kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                groupBox4.Controls.Add(kutu);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                kutu = new TextBox();
+                kutu.MaxLength = 1;
+                kutu.Left = i * 50 + 15;
+                kutu.Top = 20;
+                kutu.Width = 36;
+                kutu.Height = 40;
+                kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                kutu.Multiline = true;
+                kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                groupBox5.Controls.Add(kutu);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                kutu = new TextBox();
+                kutu.MaxLength = 1;
+                kutu.Left = i * 50 + 15;
+                kutu.Top = 20;
+                kutu.Width = 36;
+                kutu.Height = 40;
+                kutu.Text = "";
+
+                kutu.TextAlign = HorizontalAlignment.Center;
+                kutu.Multiline = true;
+                kutu.Font = new Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+                groupBox6.Controls.Add(kutu);
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            string tahmin;
+            foreach (Control item in groupBox1.Controls)
+            {
+                textBox1.AppendText(item.Text);
+            }
+            tahmin = textBox1.Text;
+
+            foreach (Control item in groupBox1.Controls)
+            {
+
+                for (int i = 0; i < tahmin.Length; i++)
+                {
+                    if (item.Text == label1.Text.Substring(i, 1))
+                    {
+                        item.BackColor = Color.Red;
+                       
+                    }
+                }
+            }
+        }
+    }
+}
